1b2e278fc913bd8477a1ab4090845741679473f0
Nathan Daniels <nathandanielsbassoon@gmail.com>
2021-09-24 16:08:01 -0500

    Added styling

--- src/components/Solfege.js
+++ src/components/Solfege.js
@@ -3,14 +3,14 @@ import '../App.css';
 const Solfege = () => {
     return (
         <div id="solfege-box">
-            <div className="syllable" id="do">Do</div>
-            <div className="syllable" id="re">Re</div>
-            <div className="syllable" id="mi">Mi</div>
-            <div className="syllable" id="fa">Fa</div>
-            <div className="syllable" id="sol">Sol</div>
-            <div className="syllable" id="la">La</div>
-            <div className="syllable" id="ti">Ti</div>
-            <div className="syllable" id="do2">Do</div>
+            <div className="syllable" id="do"><h2>Do</h2></div>
+            <div className="syllable" id="re"><h2>Re</h2></div>
+            <div className="syllable" id="mi"><h2>Mi</h2></div>
+            <div className="syllable" id="fa"><h2>Fa</h2></div>
+            <div className="syllable" id="sol"><h2>Sol</h2></div>
+            <div className="syllable" id="la"><h2>La</h2></div>
+            <div className="syllable" id="ti"><h2>Ti</h2></div>
+            <div className="syllable" id="do2"><h2>Do</h2></div>
         </div>
     )
 }
