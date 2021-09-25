619a72f66084a5ef7f90084c87659ae9fb41b534
Nathan Daniels <nathandanielsbassoon@gmail.com>
2021-09-25 12:57:42 -0500

    Converted audio files from m4a to mp3

--- src/data.js
+++ src/data.js
@@ -2,468 +2,468 @@ const keys = {
     'c-key': {
         id: 'c-key',
         name: 'C',
-        note: 'c3.m4a',
+        note: 'c3.mp3',
         solfege: {
             do: {
                 name: 'C',
-                note: 'c3.m4a'
+                note: 'c3.mp3'
             },
             re: {
                 name: 'D',
-                note: 'd3.m4a'
+                note: 'd3.mp3'
             },
             mi: {
                 name: 'E',
-                note: 'e3.m4a'
+                note: 'e3.mp3'
             },
             fa: {
                 name: 'F',
-                note: 'f3.m4a'
+                note: 'f3.mp3'
             },
             sol: {
                 name: 'G',
-                note: 'g3.m4a'
+                note: 'g3.mp3'
             },
             la: {
                 name: 'A',
-                note: 'a3.m4a'
+                note: 'a3.mp3'
             },
             ti: {
                 name: 'B',
-                note: 'b3.m4a'
+                note: 'b3.mp3'
             },
             do2: {
                 name: 'C',
-                note: 'c4.m4a'
+                note: 'c4.mp3'
             }
         }
     },
     'd-flat-key': {
         id: 'd-flat-key',
         name: 'C\u266F/D\u266D',
-        note: 'dflat3.m4a',
+        note: 'dflat3.mp3',
         solfege: {
             do: {
                 name: 'C\u266F/D\u266D',
-                note: 'dflat3.m4a'
+                note: 'dflat3.mp3'
             },
             re: {
                 name: 'D\u266F/E\u266D',
-                note: 'eflat3.m4a'
+                note: 'eflat3.mp3'
             },
             mi: {
                 name: 'E\u266F/F',
-                note: 'f3.m4a'
+                note: 'f3.mp3'
             },
             fa: {
                 name: 'F\u266F/G\u266D',
-                note: 'gflat3.m4a'
+                note: 'gflat3.mp3'
             },
             sol: {
                 name: 'G\u266F/A\u266D',
-                note: 'aflat3.m4a'
+                note: 'aflat3.mp3'
             },
             la: {
                 name: 'A\u266F/B\u266D',
-                note: 'bflat3.m4a'
+                note: 'bflat3.mp3'
             },
             ti: {
                 name: 'B\u266F/C',
-                note: 'c4.m4a'
+                note: 'c4.mp3'
             },
             do2: {
                 name: 'C\u266F/D\u266D',
-                note: 'dflat4.m4a'
+                note: 'dflat4.mp3'
             }
         }
     },
     'd-key': {
         id: 'd-key',
         name: 'D',
-        note: 'd3.m4a',
+        note: 'd3.mp3',
         solfege: {
             do: {
                 name: 'D',
-                note: 'd3.m4a'
+                note: 'd3.mp3'
             },
             re: {
                 name: 'E',
-                note: 'e3.m4a'
+                note: 'e3.mp3'
             },
             mi: {
                 name: 'F\u266F',
-                note: 'gflat3.m4a'
+                note: 'gflat3.mp3'
             },
             fa: {
                 name: 'G',
-                note: 'g3.m4a'
+                note: 'g3.mp3'
             },
             sol: {
                 name: 'A',
-                note: 'a3.m4a'
+                note: 'a3.mp3'
             },
             la: {
                 name: 'B',
-                note: 'b3.m4a'
+                note: 'b3.mp3'
             },
             ti: {
                 name: 'C\u266F',
-                note: 'dflat4.m4a'
+                note: 'dflat4.mp3'
             },
             do2: {
                 name: 'D',
-                note: 'd4.m4a'
+                note: 'd4.mp3'
             }
         }
     },
     'e-flat-key': {
         id: 'e-flat-key',
         name: 'E\u266D',
-        note: 'eflat3.m4a',
+        note: 'eflat3.mp3',
         solfege: {
             do: {
                 name: 'E\u266D',
-                note: 'eflat3.m4a'
+                note: 'eflat3.mp3'
             },
             re: {
                 name: 'F',
-                note: 'f3.m4a'
+                note: 'f3.mp3'
             },
             mi: {
                 name: 'G',
-                note: 'g3.m4a'
+                note: 'g3.mp3'
             },
             fa: {
                 name: 'A\u266D',
-                note: 'aflat3.m4a'
+                note: 'aflat3.mp3'
             },
             sol: {
                 name: 'B\u266D',
-                note: 'bflat3.m4a'
+                note: 'bflat3.mp3'
             },
             la: {
                 name: 'C',
-                note: 'c4.m4a'
+                note: 'c4.mp3'
             },
             ti: {
                 name: 'D',
-                note: 'd4.m4a'
+                note: 'd4.mp3'
             },
             do2: {
                 name: 'E\u266D',
-                note: 'eflat4.m4a'
+                note: 'eflat4.mp3'
             }
         }
     },
     'e-key': {
         id: 'e-key',
         name: 'E',
-        note: 'e3.m4a',
+        note: 'e3.mp3',
         solfege: {
             do: {
                 name: 'E',
-                note: 'e3.m4a'
+                note: 'e3.mp3'
             },
             re: {
                 name: 'F\u266F',
-                note: 'gflat3.m4a'
+                note: 'gflat3.mp3'
             },
             mi: {
                 name: 'G\u266F',
-                note: 'aflat3.m4a'
+                note: 'aflat3.mp3'
             },
             fa: {
                 name: 'A',
-                note: 'a3.m4a'
+                note: 'a3.mp3'
             },
             sol: {
                 name: 'B',
-                note: 'b3.m4a'
+                note: 'b3.mp3'
             },
             la: {
                 name: 'C\u266F',
-                note: 'dflat4.m4a'
+                note: 'dflat4.mp3'
             },
             ti: {
                 name: 'D\u266F',
-                note: 'eflat4.m4a'
+                note: 'eflat4.mp3'
             },
             do2: {
                 name: 'E',
-                note: 'e4.m4a'
+                note: 'e4.mp3'
             }
         }
     },
     'f-key': {
         id: 'f-key',
         name: 'F',
-        note: 'f3.m4a',
+        note: 'f3.mp3',
         solfege: {
             do: {
                 name: 'F',
-                note: 'f3.m4a'
+                note: 'f3.mp3'
             },
             re: {
                 name: 'G',
-                note: 'g3.m4a'
+                note: 'g3.mp3'
             },
             mi: {
                 name: 'A',
-                note: 'a3.m4a'
+                note: 'a3.mp3'
             },
             fa: {
                 name: 'B\u266D',
-                note: 'bflat3.m4a'
+                note: 'bflat3.mp3'
             },
             sol: {
                 name: 'C',
-                note: 'c4.m4a'
+                note: 'c4.mp3'
             },
             la: {
                 name: 'D',
-                note: 'd4.m4a'
+                note: 'd4.mp3'
             },
             ti: {
                 name: 'E',
-                note: 'e4.m4a'
+                note: 'e4.mp3'
             },
             do2: {
                 name: 'F',
-                note: 'f4.m4a'
+                note: 'f4.mp3'
             }
         }
     },
     'g-flat-key': {
         id: 'g-flat-key',
         name: 'F\u266F/G\u266D',
-        note: 'gflat3.m4a',
+        note: 'gflat3.mp3',
         solfege: {
             do: {
                 name: 'F\u266F/G\u266D',
-                note: 'gflat3.m4a'
+                note: 'gflat3.mp3'
             },
             re: {
                 name: 'G\u266F/A\u266D',
-                note: 'aflat3.m4a'
+                note: 'aflat3.mp3'
             },
             mi: {
                 name: 'A\u266F/B\u266D',
-                note: 'bflat3.m4a'
+                note: 'bflat3.mp3'
             },
             fa: {
                 name: 'B/C\u266D',
-                note: 'b3.m4a'
+                note: 'b3.mp3'
             },
             sol: {
                 name: 'C\u266F/D\u266D',
-                note: 'dflat4.m4a'
+                note: 'dflat4.mp3'
             },
             la: {
                 name: 'D\u266F/E\u266D',
-                note: 'eflat4.m4a'
+                note: 'eflat4.mp3'
             },
             ti: {
                 name: 'E\u266F/F',
-                note: 'f4.m4a'
+                note: 'f4.mp3'
             },
             do2: {
                 name: 'F\u266F/G\u266D',
-                note: 'gflat4.m4a'
+                note: 'gflat4.mp3'
             }
         }
     },
     'g-key': {
         id: 'g-key',
         name: 'G',
-        note: 'g3.m4a',
+        note: 'g3.mp3',
         solfege: {
             do: {
                 name: 'G',
-                note: 'g3.m4a'
+                note: 'g3.mp3'
             },
             re: {
                 name: 'A',
-                note: 'a3.m4a'
+                note: 'a3.mp3'
             },
             mi: {
                 name: 'B',
-                note: 'b3.m4a'
+                note: 'b3.mp3'
             },
             fa: {
                 name: 'C',
-                note: 'c4.m4a'
+                note: 'c4.mp3'
             },
             sol: {
                 name: 'D',
-                note: 'd4.m4a'
+                note: 'd4.mp3'
             },
             la: {
                 name: 'E',
-                note: 'e4.m4a'
+                note: 'e4.mp3'
             },
             ti: {
                 name: 'F\u266F',
-                note: 'gflat4.m4a'
+                note: 'gflat4.mp3'
             },
             do2: {
                 name: 'G',
-                note: 'g4.m4a'
+                note: 'g4.mp3'
             }
         }
     },
     'a-flat-key': {
         id: 'a-flat-key',
         name: 'A\u266D',
-        note: 'aflat3.m4a',
+        note: 'aflat3.mp3',
         solfege: {
             do: {
                 name: 'A\u266D',
-                note: 'aflat3.m4a'
+                note: 'aflat3.mp3'
             },
             re: {
                 name: 'B\u266D',
-                note: 'bflat3.m4a'
+                note: 'bflat3.mp3'
             },
             mi: {
                 name: 'C',
-                note: 'c4.m4a'
+                note: 'c4.mp3'
             },
             fa: {
                 name: 'D\u266D',
-                note: 'dflat4.m4a'
+                note: 'dflat4.mp3'
             },
             sol: {
                 name: 'E\u266D',
-                note: 'eflat4.m4a'
+                note: 'eflat4.mp3'
             },
             la: {
                 name: 'F',
-                note: 'f4.m4a'
+                note: 'f4.mp3'
             },
             ti: {
                 name: 'G',
-                note: 'g4.m4a'
+                note: 'g4.mp3'
             },
             do2: {
                 name: 'A\u266D',
-                note: 'aflat4.m4a'
+                note: 'aflat4.mp3'
             }
         }
     },
     'a-key': {
         id: 'a-key',
         name: 'A',
-        note: 'a3.m4a',
+        note: 'a3.mp3',
         solfege: {
             do: {
                 name: 'A',
-                note: 'a3.m4a'
+                note: 'a3.mp3'
             },
             re: {
                 name: 'B',
-                note: 'b3.m4a'
+                note: 'b3.mp3'
             },
             mi: {
                 name: 'C\u266F',
-                note: 'dflat4.m4a'
+                note: 'dflat4.mp3'
             },
             fa: {
                 name: 'D',
-                note: 'd4.m4a'
+                note: 'd4.mp3'
             },
             sol: {
                 name: 'E',
-                note: 'e4.m4a'
+                note: 'e4.mp3'
             },
             la: {
                 name: 'F\u266F',
-                note: 'gflat4.m4a'
+                note: 'gflat4.mp3'
             },
             ti: {
                 name: 'G\u266F',
-                note: 'aflat4.m4a'
+                note: 'aflat4.mp3'
             },
             do2: {
                 name: 'A',
-                note: 'a4.m4a'
+                note: 'a4.mp3'
             }
         }
     },
     'b-flat-key': {
         id: 'b-flat-key',
         name: 'B\u266D',
-        note: 'bflat3.m4a',
+        note: 'bflat3.mp3',
         solfege: {
             do: {
                 name: 'B\u266D',
-                note: 'bflat3.m4a'
+                note: 'bflat3.mp3'
             },
             re: {
                 name: 'C',
-                note: 'c4.m4a'
+                note: 'c4.mp3'
             },
             mi: {
                 name: 'D',
-                note: 'd4.m4a'
+                note: 'd4.mp3'
             },
             fa: {
                 name: 'E\u266D',
-                note: 'eflat4.m4a'
+                note: 'eflat4.mp3'
             },
             sol: {
                 name: 'F',
-                note: 'f4.m4a'
+                note: 'f4.mp3'
             },
             la: {
                 name: 'G',
-                note: 'g4.m4a'
+                note: 'g4.mp3'
             },
             ti: {
                 name: 'A',
-                note: 'a4.m4a'
+                note: 'a4.mp3'
             },
             do2: {
                 name: 'B\u266D',
-                note: 'bflat4.m4a'
+                note: 'bflat4.mp3'
             }
         }
     },
     'b-key': {
         id: 'b-key',
         name: 'B/C\u266D',
-        note: 'b3.m4a',
+        note: 'b3.mp3',
         solfege: {
             do: {
                 name: 'B/C\u266D',
-                note: 'b3.m4a'
+                note: 'b3.mp3'
             },
             re: {
                 name: 'C\u266F/D\u266D',
-                note: 'dflat4.m4a'
+                note: 'dflat4.mp3'
             },
             mi: {
                 name: 'D\u266F/E\u266D',
-                note: 'eflat4.m4a'
+                note: 'eflat4.mp3'
             },
             fa: {
                 name: 'E/F\u266D',
-                note: 'e4.m4a'
+                note: 'e4.mp3'
             },
             sol: {
                 name: 'F\u266F/G\u266D',
-                note: 'gflat4.m4a'
+                note: 'gflat4.mp3'
             },
             la: {
                 name: 'G\u266F/A\u266D',
-                note: 'aflat4.m4a'
+                note: 'aflat4.mp3'
             },
             ti: {
                 name: 'A\u266F/B\u266D',
-                note: 'bflat4.m4a'
+                note: 'bflat4.mp3'
             },
             do2: {
                 name: 'B/C\u266D',
-                note: 'b4.m4a'
+                note: 'b4.mp3'
             }
         }
     }
