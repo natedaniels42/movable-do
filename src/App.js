432a5aac2bc1e2b53b3ec32fd7b8e0f36e500f09
Nathan Daniels <nathandanielsbassoon@gmail.com>
2021-09-24 19:56:31 -0500

    Added handleKeyClick to App.js and connected it to Keyboard.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
+import React, { useState } from 'react';
 import Keyboard from './components/Keyboard';
 import Solfege from './components/Solfege';
+import keys from './data';
 import './App.css';
 
 const App = () => {
+  const [current, setCurrent] = useState(keys['c-key']);
+  
+  const handleKeyClick = ({ target }) => {
+    setCurrent(keys[target.id]);
+  }
+
   return (
     <div className="App">
       <h1>Movable Do</h1>
       <p>Click a key on the keyboard to set your "Do"</p>
-      <Keyboard />
+      <Keyboard handleKeyClick={handleKeyClick}/>
       <Solfege />
     </div>
   );
--- src/components/Keyboard.js
+++ src/components/Keyboard.js
@@ -2,14 +2,8 @@ import React, { useState } from 'react';
 
 import '../App.css';
 
-const Keyboard = () => {
-    const[note, setNote] = useState('')
-
-    const handleClick = (note) => {
-        new Audio(note).play();
-        setNote('C\u266F/D\u266D')
-    }
-    
+const Keyboard = (props) => {    
+    const { handleKeyClick } = props;
     return (
         <div id="keyboard">
             <svg version="1.0" xmlns="http://www.w3.org/2000/svg"
@@ -17,21 +11,21 @@ const Keyboard = () => {
             preserveAspectRatio="xMidYMid meet">
                 <g transform="translate(0.000000,378.000000) scale(0.100000,-0.100000)"
                 fill="#000000" stroke="none">
-                    <path onClick={() => handleClick('c3.m4a')} className="key" id="c-key" d="M 10 20 L 10 2020 L 340 2020 L 340 1020 L 460 1020 L 460 20 L 10 20"></path>
-                    <path onClick={() => handleClick('dflat3.m4a')} className="black key" id="d-flat-key" d="M 340 1020 L 580 1020 L 580 2020 L 340 2020 L 340 1020"></path>
-                    <path onClick={() => handleClick('d3.m4a')} className="key" id="d-key" d="M 460 20 L 910 20 L 910 1020 L 790 1020 L 790 2020 L 580 2020 L 580 1020 L 460 1020 L 460 20"></path>
-                    <path onClick={() => handleClick('eflat3.m4a')} className="black key" id="e-flat-key" d="M 790 1020 L 1030 1020 L 1030 2020 L 790 2020 L 790 1020"></path>
-                    <path onClick={() => handleClick('e3.m4a')} className="key" id="e-key" d="M 910 20 L 1360 20 L 1360 2020 L 1030 2020 L 1030 1020 L 910 1020 L 910 20"></path>
-                    <path onClick={() => handleClick('f3.m4a')} className="key" id="f-key" d="M 1360 20 L 1810 20 L 1810 1020 L 1690 1020 L 1690 2020 L 1360 2020 L 1360 20"></path> 
-                    <path onClick={() => handleClick('gflat3.m4a')} className="black key" id="g-flat-key" d="M 1690 1020 L 1930 1020 L 1930 2020 L 1690 2020 L 1690 1020"></path>
-                    <path onClick={() => handleClick('g3.m4a')} className="key" id="g-key" d="M 1810 20 L 2260 20 L 2260 1020 L 2140 1020 L 2140 2020 L 1930 2020 L 1930 1020 L 1810 1020 L 1810 20"></path>
-                    <path onClick={() => handleClick('aflat3.m4a')} className="black key" id="a-flat-key" d="M 2140 1020 L 2380 1020 L 2380 2020 L 2140 2020 L 2140 1020"></path>
-                    <path onClick={() => handleClick('a3.m4a')} className="key" id="a-key" d="M 2260 20 L 2710 20 L 2710 1020 L 2590 1020 L 2590 2020 L 2380 2020 L 2380 1020 L 2260 1020 L 2260 20"></path>
-                    <path onClick={() => handleClick('bflat3.m4a')} className="black key" id="b-flat-key" d="M 2590 1020 L 2830 1020 L 2830 2020 L 2590 2020 L 2590 1020"></path>
-                    <path onClick={() => handleClick('b3.m4a')} className="key" id="b-key" d="M 2710 20 L 3160 20 L 3160 2020 L 2830 2020 L 2830 1020 L 2710 1020 L 2710 20"></path>
+                    <path onClick={handleKeyClick} className="key" id="c-key" d="M 10 20 L 10 2020 L 340 2020 L 340 1020 L 460 1020 L 460 20 L 10 20"></path>
+                    <path onClick={handleKeyClick} className="black key" id="d-flat-key" d="M 340 1020 L 580 1020 L 580 2020 L 340 2020 L 340 1020"></path>
+                    <path onClick={handleKeyClick} className="key" id="d-key" d="M 460 20 L 910 20 L 910 1020 L 790 1020 L 790 2020 L 580 2020 L 580 1020 L 460 1020 L 460 20"></path>
+                    <path onClick={handleKeyClick} className="black key" id="e-flat-key" d="M 790 1020 L 1030 1020 L 1030 2020 L 790 2020 L 790 1020"></path>
+                    <path onClick={handleKeyClick} className="key" id="e-key" d="M 910 20 L 1360 20 L 1360 2020 L 1030 2020 L 1030 1020 L 910 1020 L 910 20"></path>
+                    <path onClick={handleKeyClick} className="key" id="f-key" d="M 1360 20 L 1810 20 L 1810 1020 L 1690 1020 L 1690 2020 L 1360 2020 L 1360 20"></path> 
+                    <path onClick={handleKeyClick} className="black key" id="g-flat-key" d="M 1690 1020 L 1930 1020 L 1930 2020 L 1690 2020 L 1690 1020"></path>
+                    <path onClick={handleKeyClick} className="key" id="g-key" d="M 1810 20 L 2260 20 L 2260 1020 L 2140 1020 L 2140 2020 L 1930 2020 L 1930 1020 L 1810 1020 L 1810 20"></path>
+                    <path onClick={handleKeyClick} className="black key" id="a-flat-key" d="M 2140 1020 L 2380 1020 L 2380 2020 L 2140 2020 L 2140 1020"></path>
+                    <path onClick={handleKeyClick} className="key" id="a-key" d="M 2260 20 L 2710 20 L 2710 1020 L 2590 1020 L 2590 2020 L 2380 2020 L 2380 1020 L 2260 1020 L 2260 20"></path>
+                    <path onClick={handleKeyClick} className="black key" id="b-flat-key" d="M 2590 1020 L 2830 1020 L 2830 2020 L 2590 2020 L 2590 1020"></path>
+                    <path onClick={handleKeyClick} className="key" id="b-key" d="M 2710 20 L 3160 20 L 3160 2020 L 2830 2020 L 2830 1020 L 2710 1020 L 2710 20"></path>
                 </g>
             </svg>
-            <p id="note">{note}</p>
+            <p id="note"></p>
         </div>
     )
 }
