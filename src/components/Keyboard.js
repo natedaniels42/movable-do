7eec13d42422ef933cefcf04de972e6bba39bbec
Nathan Daniels <nathandanielsbassoon@gmail.com>
2021-09-25 14:22:46 -0500

    More CSS

--- src/components/Keyboard.js
+++ src/components/Keyboard.js
@@ -10,7 +10,7 @@ const Keyboard = (props) => {
             preserveAspectRatio="xMidYMid meet">
                 <g transform="translate(0.000000,228.000000) scale(0.100000,-0.100000)"
                 fill="#000000" stroke="none">
-                    <path onClick={handleKeyClick} className="key" id="c-key" d="M 10 20 L 10 2020 L 340 2020 L 340 1020 L 460 1020 L 460 20 L 10 20"></path>
+                    <path onClick={handleKeyClick} className="key default" id="c-key" d="M 10 20 L 10 2020 L 340 2020 L 340 1020 L 460 1020 L 460 20 L 10 20"></path>
                     <path onClick={handleKeyClick} className="black key" id="d-flat-key" d="M 340 1020 L 580 1020 L 580 2020 L 340 2020 L 340 1020"></path>
                     <path onClick={handleKeyClick} className="key" id="d-key" d="M 460 20 L 910 20 L 910 1020 L 790 1020 L 790 2020 L 580 2020 L 580 1020 L 460 1020 L 460 20"></path>
                     <path onClick={handleKeyClick} className="black key" id="e-flat-key" d="M 790 1020 L 1030 1020 L 1030 2020 L 790 2020 L 790 1020"></path>
@@ -24,6 +24,7 @@ const Keyboard = (props) => {
                     <path onClick={handleKeyClick} className="key" id="b-key" d="M 2710 20 L 3160 20 L 3160 2020 L 2830 2020 L 2830 1020 L 2710 1020 L 2710 20"></path>
                 </g>
             </svg>
+            <p className="text">Current Key:</p>
             <p id="note">{current.name}</p>
         </div>
     )
