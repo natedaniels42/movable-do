ba50a8a857efef83eb3e5eceff2fa0ed8432b2e6
Nathan Daniels <nathandanielsbassoon@gmail.com>
2021-09-24 22:29:32 -0500

    Css tweaks

--- src/components/Keyboard.js
+++ src/components/Keyboard.js
@@ -7,7 +7,7 @@ const Keyboard = (props) => {
     return (
         <div id="keyboard">
             <svg version="1.0" xmlns="http://www.w3.org/2000/svg"
-            width="600.000000pt" height="278.000000pt" viewBox="0 0 600.000000 378.000000"
+            width="100%" height="50%" viewBox="0 0 600.000000 378.000000"
             preserveAspectRatio="xMidYMid meet">
                 <g transform="translate(0.000000,378.000000) scale(0.100000,-0.100000)"
                 fill="#000000" stroke="none">
