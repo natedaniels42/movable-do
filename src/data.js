bcd66825b85a605e1eff6ef61f900539235e40a0
Nathan Daniels <nathandanielsbassoon@gmail.com>
2021-09-26 08:57:42 -0500

    Revert "Updated data to fix bug"
    This reverts commit 8ee0e184d16eb06408334785f0c124b96dc775ac.

--- src/data.js
+++ src/data.js
@@ -4,51 +4,38 @@ const keys = {
         name: 'C',
         note: 'c3.mp3',
         solfege: {
-            notes: {
-                do: 'c3.mp3',
-                re: 'd3.mp3',
-                me: 'eflat3.mp3',
-                mi: 'e3.mp3',
-                fa: 'f3.mp3',
-                sol: 'g3.mp3',
-                le: 'aflat3.mp3',
-                la: 'a3.mp3',
-                ta: 'bflat3.mp3',
-                ti: 'b3.mp3',
-                do2: 'c4.mp3'
-            },
             major: {
                 do: {
                     name: 'C',
-                    note: 'c3.mp3'                
+                    note: 'c3.mp3'
                 },
                 re: {
                     name: 'D',
-                    note: 'd3.mp3'                
+                    note: 'd3.mp3'
                 },
                 mi: {
                     name: 'E',
-                    note: 'e3.mp3'                
+                    note: 'e3.mp3'
                 },
                 fa: {
                     name: 'F',
-                    note: 'f3.mp3'                
+                    note: 'f3.mp3'
                 },
                 sol: {
                     name: 'G',
-                    note: 'g3.mp3'                
+                    note: 'g3.mp3'
                 },
                 la: {
                     name: 'A',
-                    note: 'a3.mp3'                
+                    note: 'a3.mp3'
                 },
                 ti: {
                     name: 'B',
-                    note: 'b3.mp3'                
+                    note: 'b3.mp3'
                 },
                 do2: {
                     name: 'C',
-                    note: 'c4.mp3'                
+                    note: 'c4.mp3'
                 }
             },
             minor: {
@@ -162,19 +149,6 @@ const keys = {
         name: 'C\u266F/D\u266D',
         note: 'dflat3.mp3',
         solfege: {
-            notes: {
-                do: 'dflat3.mp3',
-                re: 'eflat3.mp3',
-                me: 'e3.mp3',
-                mi: 'f3.mp3',
-                fa: 'gflat3.mp3',
-                sol: 'aflat3.mp3',
-                le: 'a3.mp3',
-                la: 'bflat3.mp3',
-                ta: 'b3.mp3',
-                ti: 'c4.mp3',
-                do2: 'dflat4.mp3'
-            },
             major: {
 
                 do: {
@@ -321,20 +295,8 @@ const keys = {
         name: 'D',
         note: 'd3.mp3',
         solfege: {
-            notes: {
-                do: 'd3.mp3',
-                re: 'e3.mp3',
-                me: 'f3.mp3',
-                mi: 'gflat3.mp3',
-                fa: 'g3.mp3',
-                sol: 'a3.mp3',
-                le: 'bflat3.mp3',
-                la: 'b3.mp3',
-                ta: 'c4.mp3',
-                ti: 'dflat4.mp3',
-                do2: 'd4.mp3'
-            },
             major: {
+
                 do: {
                     name: 'D',
                     note: 'd3.mp3'
@@ -479,20 +441,8 @@ const keys = {
         name: 'D\u266F/E\u266D',
         note: 'eflat3.mp3',
         solfege: {
-            notes: {
-                do: 'eflat3.mp3',
-                re: 'f3.mp3',
-                me: 'gflat3.mp3',
-                mi: 'g3.mp3',
-                fa: 'aflat3.mp3',
-                sol: 'bflat3.mp3',
-                le: 'b3.mp3',
-                la: 'c4.mp3',
-                ta: 'dflat4.mp3',
-                ti: 'd4.mp3',
-                do2: 'eflat4.mp3'
-            },
             major: {
+
                 do: {
                     name: 'E\u266D',
                     note: 'eflat3.mp3'
@@ -637,20 +587,8 @@ const keys = {
         name: 'E',
         note: 'e3.mp3',
         solfege: {
-            notes: {
-                do: 'e3.mp3',
-                re: 'gflat3.mp3',
-                me: 'g3.mp3',
-                mi: 'aflat3.mp3',
-                fa: 'a3.mp3',
-                sol: 'b3.mp3',
-                le: 'c4.mp3',
-                la: 'dflat4.mp3',
-                ta: 'd4.mp3',
-                ti: 'eflat4.mp3',
-                do2: 'e4.mp3'
-            },
             major: {
+
                 do: {
                     name: 'E',
                     note: 'e3.mp3'
@@ -795,20 +733,8 @@ const keys = {
         name: 'F',
         note: 'f3.mp3',
         solfege: {
-            notes: {
-                do: 'f3.mp3',
-                re: 'g3.mp3',
-                me: 'aflat3.mp3',
-                mi: 'a3.mp3',
-                fa: 'bflat3.mp3',
-                sol: 'c4.mp3',
-                le: 'dflat4.mp3',
-                la: 'd4.mp3',
-                ta: 'eflat4.mp3',
-                ti: 'e4.mp3',
-                do2: 'f4.mp3'
-            },
             major: {
+
                 do: {
                     name: 'F',
                     note: 'f3.mp3'
@@ -953,20 +879,8 @@ const keys = {
         name: 'F\u266F/G\u266D',
         note: 'gflat3.mp3',
         solfege: {
-            notes: {
-                do: 'gflat3.mp3',
-                re: 'aflat3.mp3',
-                me: 'a3.mp3',
-                mi: 'bflat3.mp3',
-                fa: 'b3.mp3',
-                sol: 'dflat4.mp3',
-                le: 'd4.mp3',
-                la: 'eflat4.mp3',
-                ta: 'e4.mp3',
-                ti: 'f4.mp3',
-                do2: 'gflat4.mp3'
-            },
             major: {
+
                 do: {
                     name: 'F\u266F/G\u266D',
                     note: 'gflat3.mp3'
@@ -1111,20 +1025,8 @@ const keys = {
         name: 'G',
         note: 'g3.mp3',
         solfege: {
-            notes: {
-                do: 'g3.mp3',
-                re: 'a3.mp3',
-                me: 'bflat3.mp3',
-                mi: 'b3.mp3',
-                fa: 'c4.mp3',
-                sol: 'd4.mp3',
-                le: 'eflat4.mp3',
-                la: 'e4.mp3',
-                ta: 'f4.mp3',
-                ti: 'gflat4.mp3',
-                do2: 'g4.mp3'
-            },
             major: {
+
                 do: {
                     name: 'G',
                     note: 'g3.mp3'
@@ -1269,20 +1171,8 @@ const keys = {
         name: 'G\u266F/A\u266D',
         note: 'aflat3.mp3',
         solfege: {
-            notes: {
-                do: 'aflat3.mp3',
-                re: 'bflat3.mp3',
-                me: 'b3.mp3',
-                mi: 'c4.mp3',
-                fa: 'dflat4.mp3',
-                sol: 'eflat4.mp3',
-                le: 'e4.mp3',
-                la: 'f4.mp3',
-                ta: 'gflat4.mp3',
-                ti: 'g4.mp3',
-                do2: 'aflat4.mp3'
-            },
             major: {
+
                 do: {
                     name: 'A\u266D',
                     note: 'aflat3.mp3'
@@ -1411,7 +1301,7 @@ const keys = {
                         note: 'f4.mp3'
                     },
                     ti: {
-                        name: 'F\uD834\uDD2A/G',
+                        name: 'F\uD834\uDD2A\G',
                         note: 'g4.mp3'
                     },
                     do2: {
@@ -1427,20 +1317,8 @@ const keys = {
         name: 'A',
         note: 'a3.mp3',
         solfege: {
-            notes: {
-                do: 'a3.mp3',
-                re: 'b3.mp3',
-                me: 'c4.mp3',
-                mi: 'dflat4.mp3',
-                fa: 'd4.mp3',
-                sol: 'e4.mp3',
-                le: 'f4.mp3',
-                la: 'gflat4.mp3',
-                ta: 'g4.mp3',
-                ti: 'aflat4.mp3',
-                do2: 'a4.mp3'
-            },
             major: {
+
                 do: {
                     name: 'A',
                     note: 'a3.mp3'
@@ -1585,19 +1463,6 @@ const keys = {
         name: 'A\u266F/B\u266D',
         note: 'bflat3.mp3',
         solfege: {
-            notes: {
-                do: 'bflat3.mp3',
-                re: 'c4.mp3',
-                me: 'dflat4.mp3',
-                mi: 'd4.mp3',
-                fa: 'eflat4.mp3',
-                sol: 'f4.mp3',
-                le: 'gflat4.mp3',
-                la: 'g4.mp3',
-                ta: 'aflat4.mp3',
-                ti: 'a4.mp3',
-                do2: 'bflat4.mp3'
-            },
             major: {
                 do: {
                     name: 'B\u266D',
@@ -1743,20 +1608,8 @@ const keys = {
         name: 'B/C\u266D',
         note: 'b3.mp3',
         solfege: {
-            notes: {
-                do: 'b3.mp3',
-                re: 'dflat4.mp3',
-                me: 'd4.mp3',
-                mi: 'eflat4.mp3',
-                fa: 'e4.mp3',
-                sol: 'gflat4.mp3',
-                le: 'g4.mp3',
-                la: 'aflat4.mp3',
-                ta: 'a4.mp3',
-                ti: 'bflat4.mp3',
-                do2: 'b4.mp3'
-            },
             major: {
+
                 do: {
                     name: 'B/C\u266D',
                     note: 'b3.mp3'
